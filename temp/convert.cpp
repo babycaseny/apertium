cbb396f179646b1069eb043681b74eca4ed469d5
cirikDellKoc <cirikdellkoc@ubuntu.(none)>
2013-05-01 23:59:13 +0300

    sliding window challenge v2

--- temp/convert.cpp
+++ temp/convert.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h> 
 
 #include <vector>
 #include <string>
 #include <iostream>
 #include <algorithm>
-#include <queue>
-#include <set>
-#include <map>
 
 using namespace std;
+vector< vector<string> >TOKEN_LIST;
 
+void debug()
+{
+  for(int i=0;i<TOKEN_LIST.size();i++)
+    {
+      for(int j=0;j<TOKEN_LIST[i].size();j++)
+	{
+	  cout << TOKEN_LIST[i][j];
+	  cout << " ";
+	}
+      cout << endl;
+    }
+}
 
-void read(char *arguments[])
+bool read(char *arguments[])
 {
   FILE *file = fopen(arguments[1],"r");
+  string opt_raw = arguments[2];
+  bool opt;
+  if(opt_raw.compare("-r") == 0)opt = true;
+  else if(opt_raw.compare("-f") == 0)opt = false;
+  else
+    {
+      cout << "Cannot recognize the option!";
+      exit(0);   
+    }
   if(file == NULL)
     {
       cout << "Cannot open the file!";
@@ -23,8 +43,10 @@ void read(char *arguments[])
     }
 
   char temp;
-  string token;
+
   bool skip_all=true,start_convert=false;
+  vector<string>tokens;
+  string token;
 
   while(1)
     {
@@ -35,7 +57,13 @@ void read(char *arguments[])
       if(temp == '^')
 	{
 	  skip_all = false;
-	  cout << " ";
+	  if(tokens.size() >0)
+	    {
+	      TOKEN_LIST.push_back(tokens);
+	    }
+	  tokens.clear();
+	  token = "";
+
 	  continue;
 	}
       else if(temp == '/')
@@ -43,26 +71,57 @@ void read(char *arguments[])
 	  if(start_convert == false)start_convert = true;
    	  else
 	    {
-	      skip_all = true;
-	      start_convert = false;
+	      tokens.push_back(token);
+	      token = "";
 	    }
 	}
       else if(temp == '$')
 	{
 	  skip_all = true;
 	  start_convert = false;
+          tokens.push_back(token);
+	  token = "";
 	}
-
       else if(start_convert && temp!= '>')
 	{
 	  if(temp == '<')temp = '.';
-	  cout << temp;
+	  token.append(&temp);
 	}
     }
-  cout << endl;
+  TOKEN_LIST.push_back(tokens);
+  //  debug();
+  return opt;
 }
+void print()
+{
+  for(int i=0;i<TOKEN_LIST.size();i++)
+    {
+      for(int j=0;j<TOKEN_LIST[i].size();j++)
+	{
+	  cout << TOKEN_LIST[i][j];
+	  cout << " ";
+	}
+      cout << endl;
+    }
+}
+void print(bool opt)
+{
+
+  vector<int>indexes(TOKEN_LIST.size());
+  if(opt)
+    for(int i=0;i<TOKEN_LIST.size();i++)
+       indexes[i] = rand()%TOKEN_LIST[i].size();
+   for(int i=0;i<TOKEN_LIST.size();i++)
+    {
+      cout << TOKEN_LIST[i][indexes[i]];
+      cout << " ";
+    }
+   cout << endl;
+}
+
 int main( int argc, char *argv[])
 {
-  read(argv);
+  bool opt = read(argv);
+  print(opt);
   return 0;
 }
